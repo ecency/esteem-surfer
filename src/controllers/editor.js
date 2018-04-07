3c00b00bc3896967627f1980b22bb24796cc3e86
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-04-07 23:38:56 +0300

    editor improvements

--- src/controllers/editor.js
+++ src/controllers/editor.js
@@ -1,3 +1,33 @@
-export default ($scope) => {
+export default ($scope, $timeout) => {
 
+  $scope.body = '';
+
+  let bodyTimeout = null;
+
+  $scope.$watch('body', (newVal, oldVal) => {
+
+    setTimeout(() => {
+      const e = document.querySelector('#editor-preview');
+      e.scrollTop = e.scrollHeight;
+    }, 200);
+  });
+
+  $scope.$watch('tempBody', (newVal, oldVal) => {
+    if (newVal === oldVal) {
+      return;
+    }
+
+    if (newVal === $scope.realBody) {
+      return;
+    }
+
+    if (bodyTimeout !== null) {
+      $timeout.cancel(bodyTimeout);
+      bodyTimeout = null;
+    }
+
+    bodyTimeout = $timeout(() => {
+      $scope.body = newVal;
+    }, 500);
+  })
 };
--- src/directives/content-editor-controls.js
+++ src/directives/content-editor-controls.js
@@ -1,3 +1,5 @@
+import {insertText} from '../editor-utils';
+
 export default () => {
   return {
     restrict: 'E',
@@ -9,12 +11,16 @@ export default () => {
 
     },
     template: `<div class="content-editor-controls">
-        <button class="btn btn-default" ng-click="insertHeader()"><i class="fa fa-header"></i></button>
         <button class="btn btn-default" ng-click="insertBold()"><i class="fa fa-bold"></i></button>
         <button class="btn btn-default" ng-click="insertItalic()"><i class="fa fa-italic"></i></button>
+        <button class="btn btn-default" ng-click="insertHeader()"><i class="fa fa-header"></i></button>
+        <button class="btn btn-default" ng-click="insertCode()"><i class="fa fa-code"></i></button>
         <button class="btn btn-default" ng-click="insertQuote()"><i class="fa fa-quote-right"></i></button>
-        <button class="btn btn-default" ng-click="insertLink()"><i class="fa fa-link"></i></button>
+        <button class="btn btn-default" ng-click="insertOlList()"><i class="fa fa-list-ol"></i></button>
+        <button class="btn btn-default" ng-click="insertUlList()"><i class="fa fa-list-ul"></i></button>
+        <button class="btn btn-default" ng-click="insertHr()"><i class="fa fa-minus"></i></button>
         <button class="btn btn-default" ng-click="insertImage()"><i class="fa fa-image"></i></button>
+        <button class="btn btn-default" ng-click="insertLink()"><i class="fa fa-link"></i></button>
         </div>`,
     controller: ($scope, $rootScope, $timeout) => {
 
@@ -28,61 +34,44 @@ export default () => {
         txtEl.focus();
       }, 500);
 
-
-      $scope.insertSpace = () => {
-        let pos = txtEl.value.length;
-
-        txtEl.selectionStart = pos;
-        txtEl.selectionEnd = pos;
-        txtEl.focus();
-
-        document.execCommand('insertText', false, ' ');
-
-        pos = txtEl.value.length;
-        txtEl.selectionStart = pos;
-        txtEl.selectionEnd = pos;
-      };
-
-      $scope.insertText = (before, after = '') => {
-        const startPos = txtEl.selectionStart;
-        const endPos = txtEl.selectionEnd;
-        const selText = txtEl.value.substring(startPos, endPos);
-
-        let insertText = `${before}${selText}${after}`;
-
-        const newStartPos = startPos + before.length;
-        const newEndPos = newStartPos + selText.length;
-
-        txtEl.focus();
-
-        document.execCommand('insertText', false, insertText);
-
-        txtEl.selectionStart = newStartPos;
-        txtEl.selectionEnd = newEndPos;
-      };
-
-      $scope.insertHeader = () => {
-        $scope.insertText('# ', '');
-      };
-
       $scope.insertBold = () => {
-        $scope.insertText('**', '**');
+        insertText(txtEl, '**', '**');
       };
 
       $scope.insertItalic = () => {
-        $scope.insertText('*', '*');
+        insertText(txtEl, '*', '*');
+      };
+
+      $scope.insertHeader = () => {
+        insertText(txtEl, '# ', '');
+      };
+
+      $scope.insertCode = () => {
+        insertText(txtEl, '<code>', '</code>');
       };
 
       $scope.insertQuote = () => {
-        $scope.insertText('> ', '');
+        insertText(txtEl, '> ', '');
       };
 
-      $scope.insertLink = () => {
-        $scope.insertText('[', '](url)');
+      $scope.insertOlList = () => {
+        insertText(txtEl, "1. item1\n2. item2\n3. item3", '');
+      };
+
+      $scope.insertUlList = () => {
+        insertText(txtEl, "* item1\n* item2\n* item3", '');
+      };
+
+      $scope.insertHr = () => {
+        insertText(txtEl, '<hr>', '');
       };
 
       $scope.insertImage = (name = '', url = 'url') => {
-        $scope.insertText(`![${name}`, `](${url})`);
+        insertText(txtEl, `![${name}`, `](${url})`);
+      };
+
+      $scope.insertLink = () => {
+        insertText(txtEl, '[', '](url)');
       };
     }
   };
