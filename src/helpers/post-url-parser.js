a5110755fbccb0af9139d9d8e694f6463f3d0d67
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-05-15 23:02:03 +0300

    full post link support added to search. for both steemit and busy post urls

---
+++ src/helpers/post-url-parser.js
@@ -0,0 +1,47 @@
+export const postUrlParser = (url) => {
+
+  const parseSteemit = (url) => {
+    const r = /^https?:\/\/(.*)\/(.*)\/(@[\w\.\d-]+)\/(.*)/i;
+    const match = url.match(r);
+    if (match && match.length === 5) {
+      return {
+        'author': match[3].replace('@', ''),
+        'permlink': match[4]
+      }
+    }
+
+    return null;
+  };
+
+  const parseBusy = (url) => {
+    const r = /^https?:\/\/(.*)\/(@[\w\.\d-]+)\/(.*)/i;
+    const match = url.match(r);
+    if (match && match.length === 4) {
+      return {
+        'author': match[2].replace('@', ''),
+        'permlink': match[3]
+      }
+    }
+
+    return null;
+  };
+
+  if (url.startsWith('https://steemit.com')) {
+    return parseSteemit(url);
+  }
+
+  if (url.startsWith('https://busy.org')) {
+    return parseBusy(url);
+  }
+
+  // For non url's like @good-karma/esteem-london-presentation-e3105ba6637ed
+  const match = url.match(/^(@[\w\.\d-]+)\/(.*)/);
+  if (match && match.length === 3) {
+    return {
+      'author': match[1].replace('@', ''),
+      'permlink': match[2]
+    }
+  }
+
+  return null;
+};
