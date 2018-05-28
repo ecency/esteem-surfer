ec3f823592de419a4dd03257c7507c5489b18044
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-05-28 17:06:49 +0300

    showing wrong numbers for diffirent currencies bug fixed #25

--- src/filters/sum-post-total.js
+++ src/filters/sum-post-total.js
@@ -1,20 +1,16 @@
 export default ($rootScope) => {
   return (post) => {
 
-    if(!post){
+    if (!post) {
       return null;
     }
 
     let rate = $rootScope.currencyRate;
 
     if (post && post.pending_payout_value && post.last_payout === '1970-01-01T00:00:00') {
-
-      //value.total_payout_value.split(" ")[0])+parseFloat(value.total_pending_payout_value.split(" ")[0])
-      //return (parseFloat(value.pending_payout_value.split(" ")[0])*rate);
-      return post.total_payout_value ? ((parseFloat(post.total_payout_value.split(" ")[0])) + (parseFloat(post.pending_payout_value.split(" ")[0])) * rate).toFixed(2) : 0;
+      return post.total_payout_value ? ((Number(parseFloat(post.total_payout_value.split(" ")[0])) + Number(parseFloat(post.pending_payout_value.split(" ")[0]))) * rate).toFixed(2) : 0;
     } else {
-
-      return post.total_payout_value ? ((parseFloat(post.total_payout_value.split(" ")[0])) + (parseFloat(post.curator_payout_value.split(" ")[0])) * rate).toFixed(2) : 0;
+      return post.total_payout_value ? ((Number(parseFloat(post.total_payout_value.split(" ")[0])) + Number(parseFloat(post.curator_payout_value.split(" ")[0]))) * rate).toFixed(2) : 0;
     }
   };
 }
