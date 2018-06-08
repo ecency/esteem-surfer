025cf16d37a89b253152be8fa031c950d6299730
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-06-08 15:16:34 +0300

    image src proxify added to catch-post-image filter

--- src/filters/catch-post-image.js
+++ src/filters/catch-post-image.js
@@ -1,3 +1,5 @@
+import {proxifyImageSrc} from '../helpers/proxify-image-src';
+
 export const catchPostImage = (post) => {
 
   let meta = null;
@@ -8,19 +10,19 @@ export const catchPostImage = (post) => {
   }
 
   if (meta && meta.image && meta.image.length > 0) {
-    return meta.image[0];
+    return proxifyImageSrc(meta.image[0]);
   } else {
     // If no image specified in json metadata, try extract first image href from post body
     let imgReg = /\<img.+src\=(?:\"|\')(.+?)(?:\"|\')(.*)>/;
     let bodyMatch = post.body.match(imgReg);
     if (bodyMatch) {
-      return bodyMatch[1];
+      return proxifyImageSrc(bodyMatch[1]);
     } else {
       // If there is no <img> tag, check from markdown img tag ![](image.png)
       imgReg = /(?:!\[(.*?)\]\((.*?)\))/;
       bodyMatch = imgReg.exec(post.body);
       if (bodyMatch) {
-       return bodyMatch[2];
+       return proxifyImageSrc(bodyMatch[2]);
       }
     }
   }
