9da979d8c5f97f71b4c86c36aa4c126c12048dc1
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-06-17 17:07:34 +0300

    code improvements

--- src/ng-app.js
+++ src/ng-app.js
@@ -186,7 +186,7 @@ import eSteemService from './services/esteem';
 import editorService from './services/editor';
 import cryptoService from './services/crypto';
 import pinService from './services/pin'
-
+import $confirm from './services/confirm';
 
 // Filters
 import {catchPostImageFilter} from './filters/catch-post-image';
@@ -436,56 +436,7 @@ ngApp.config(($translateProvider, $routeProvider, $httpProvider) => {
   })
   .factory('cryptoService', cryptoService)
   .factory('pinService', pinService)
-  .factory('$confirm', ($uibModal, $sce) => {
-    return (title = '?', message = null, OkHandler = null, CancelHandler = null) => {
-      return $uibModal.open({
-        templateUrl: 'templates/confirm.html',
-        controller: ($scope, $uibModalInstance, Title, Message, OkHandler, CancelHandler) => {
-          $scope.title = Title;
-          $scope.message = Message ? $sce.trustAsHtml(Message) : Message;
-
-          $scope.ok = () => {
-            $scope.close();
-            if (OkHandler) {
-              OkHandler();
-            }
-          };
-
-          $scope.cancel = () => {
-            $scope.close();
-            if (CancelHandler) {
-              CancelHandler();
-            }
-          };
-
-          $scope.close = () => {
-            $uibModalInstance.dismiss('cancel');
-          };
-        },
-        windowClass: 'confirm-modal',
-        resolve: {
-          Title: () => {
-            return title;
-          },
-          Message: () => {
-            return message
-          },
-          OkHandler: () => {
-            return OkHandler
-          },
-          CancelHandler: () => {
-            return CancelHandler
-          }
-        }
-      }).result.then((data) => {
-
-      }, () => {
-
-      });
-
-
-    }
-  })
+  .factory('$confirm', $confirm)
 
   .directive('navBar', navBarDir)
   .directive('appFooter', footerDir)
