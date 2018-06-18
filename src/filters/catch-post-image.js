5a8680b23186e61d559b1df4d5b40aabb6379693
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-06-18 15:38:10 +0300

    catch post image improvements

--- src/filters/catch-post-image.js
+++ src/filters/catch-post-image.js
@@ -2,6 +2,7 @@ import {proxifyImageSrc} from '../helpers/proxify-image-src';
 
 export const catchPostImage = (post) => {
 
+  // return from json metadata if exists
   let meta = null;
 
   try {
@@ -11,21 +12,30 @@ export const catchPostImage = (post) => {
 
   if (meta && meta.image && meta.image.length > 0) {
     return proxifyImageSrc(meta.image[0]);
-  } else {
-    // If no image specified in json metadata, try extract first image href from post body
-    let imgReg = /\<img.+src\=(?:\"|\')(.+?)(?:\"|\')(.*)>/;
-    let bodyMatch = post.body.match(imgReg);
-    if (bodyMatch) {
-      return proxifyImageSrc(bodyMatch[1]);
-    } else {
-      // If there is no <img> tag, check from markdown img tag ![](image.png)
-      imgReg = /(?:!\[(.*?)\]\((.*?)\))/;
-      bodyMatch = imgReg.exec(post.body);
-      if (bodyMatch) {
-       return proxifyImageSrc(bodyMatch[2]);
-      }
-    }
   }
+
+  // try to extract images by regex
+  let imgReg2 = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpe?g|gif|png)/gim;
+  const m = post.body.match(imgReg2);
+  if (m) {
+    return proxifyImageSrc(m[0]);
+  }
+
+  // If no image specified in json metadata, try extract first image href from post body
+  let imgReg = /\<img.+src\=(?:\"|\')(.+?)(?:\"|\')(.*)>/;
+  let bodyMatch = post.body.match(imgReg);
+  if (bodyMatch) {
+    return proxifyImageSrc(bodyMatch[1]);
+  }
+
+  // If there is no <img> tag, check from markdown img tag ![](image.png)
+  imgReg = /(?:!\[(.*?)\]\((.*?)\))/;
+  bodyMatch = imgReg.exec(post.body);
+  if (bodyMatch) {
+    return proxifyImageSrc(bodyMatch[2]);
+  }
+
+
   return null;
 };
 
