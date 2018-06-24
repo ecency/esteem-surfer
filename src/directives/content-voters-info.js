b8c166a08a0d8d3fee120e7de8d072c8dbb411ae
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-06-24 15:48:33 +0300

    Voters info popover implemented #new 78

--- src/directives/content-voters-info.js
+++ src/directives/content-voters-info.js
@@ -5,12 +5,71 @@ export default () => {
     scope: {
       content: '<'
     },
-    template: `<a ng-click="votersClicked()" title="{{ voteCount }} {{ 'VOTES' | translate | lowercase }}"><i class="fa fa-users"></i> {{ voteCount }} </a>`,
-    controller: ($scope, $rootScope, $uibModal) => {
+    template: `<a ng-click="votersClicked()" uib-tooltip-html="toolTip"><i class="fa fa-users"></i> {{ voteCount }} </a>`,
+    controller: ($scope, $rootScope, $uibModal, $sce, $filter) => {
+
+      const prepareTooltip = () => {
+
+        if ($scope.content.active_votes.length === 0) {
+          $scope.toolTip = $sce.trustAsHtml($filter('__')('No votes'));
+          return;
+        }
+
+        const totalPayout =
+          parseFloat($scope.content.pending_payout_value.split(' ')[0]) +
+          parseFloat($scope.content.total_payout_value.split(' ')[0]) +
+          parseFloat($scope.content.curator_payout_value.split(' ')[0]);
+
+        const voteRshares = $scope.content.active_votes.reduce((a, b) => a + parseFloat(b.rshares), 0);
+        const ratio = totalPayout / voteRshares;
+        const rate = $rootScope.currencyRate;
+
+        const votesData = [];
+
+        for (let a of $scope.content.active_votes) {
+          const rew = ((a.rshares * ratio) * rate);
+
+          votesData.push(Object.assign({}, {
+            reward: rew,
+            reward_fixed: rew.toFixed(1)
+          }, a, {perc: (a.percent / 100).toFixed(1)}));
+        }
+
+        votesData.sort((a, b) => {
+          const keyA = a['reward'],
+            keyB = b['reward'];
+
+          if (keyA > keyB) return -1;
+          if (keyA < keyB) return 1;
+          return 0;
+        });
+
+        let toolTip = `<div style="text-align:left">`;
+        let max = 10;
+        let more = 0;
+        let i = 0;
+        for (let v of votesData) {
+          toolTip += `<strong>${v.voter}</strong>: ${v.reward_fixed }${ $filter('currencySymbol')($rootScope.currency) } -  ${v.perc }%  <br>`;
+          i += 1;
+          if (i === max) {
+            more = votesData.length - max;
+            break;
+          }
+        }
+
+        if (more) {
+          toolTip += `<strong>${ $filter('__')('and') } ${more} ${ $filter('__')('more') }</strong><br>`;
+        }
+
+        toolTip += '</div>';
+        $scope.toolTip = $sce.trustAsHtml(toolTip);
+      };
 
       const main = () => {
         if ($scope.content.active_votes) {
           $scope.voteCount = $scope.content.active_votes.length;
+
+          prepareTooltip();
         } else {
           // For search results
           $scope.voteCount = $scope.content.net_votes;
