d1c52acb20fc544cb8a8fbee88e6b4cff65f0ee0
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-07-10 21:32:30 +0300

    config.example update

--- src/config.example.js
+++ src/config.example.js
@@ -2,5 +2,6 @@ export const GH_TOKEN = 'your token here'; // github user token to check updates
 
 export default (app)=> {
   app.constant('API_END_POINT','SERVER_ADDRESS');
+  app.constant('NWS_ADDRESS','SERVER_ADDRESS');
   //please change SERVER_ADDRESS to backend API providers
 }
