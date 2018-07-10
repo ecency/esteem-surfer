22dfdd6e5e3bb0a2621fd58d09168fcf0f652968
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-07-10 17:46:45 +0300

    activity style improvements

--- src/directives/activity-list-item.js
+++ src/directives/activity-list-item.js
@@ -12,44 +12,46 @@ export default () => {
       <div class="group-title" ng-if="activity.gkf">{{ date2key(activity.gk) }}</div>
       <div class="activity-list-item" ng-click="clicked(draft)" ng-class="{'not-read': activity.read === 0}">
         
-        <a ng-click="goProfile(activity.source)" class="source-profile-pic" author-bg-img-style author="{{ activity.source }}"></a> 
+        <a ng-click="goAccount(activity.source)" class="source-profile-pic" author-bg-img-style author="{{ activity.source }}"></a> 
         
         <!-- Votes -->
         <div class="activity-content" ng-if="['vote', 'unvote'].includes(activity.type)">
           <div class="first-line">
-            <a ng-click="goProfile(activity.source)" class="source-name">{{ activity.source }}</a>  
+            <a ng-click="goAccount(activity.source)" class="source-name">{{ activity.source }}</a>  
             <span ng-if="activity.type==='vote'" class="activity-action"><i class="fa fa-thumbs-o-up"></i></span>
             <span ng-if="activity.type==='unvote'" class="activity-action"><i class="fa fa-thumbs-o-down"></i></span>
             <small>{{ activity.weight / 100 }}%</small>
           </div>
           <div class="second-line">
-            <a ng-click="goPost(activity.author, activity.permlink)" class="post-link">{{ activity.permlink }}</a>
+            <a ng-click="goContent(activity.author, activity.permlink)" class="post-link">{{ activity.permlink }}</a>
           </div>
         </div>
         
         <!-- Replies -->
         <div class="activity-content" ng-if="activity.type==='reply'" >
           <div class="first-line">
-            <a ng-click="goProfile(activity.source)" class="source-name">{{ activity.source }}</a>  
+            <a ng-click="goAccount(activity.source)" class="source-name">{{ activity.source }}</a>  
             <span class="activity-action"><i class="fa fa-comment-o"></i></span>
-            <a ng-click="goPost(activity.author, activity.permlink)" class="post-link">{{ activity.parent_permlink }}</a> 
+            <a ng-click="goContent(activity.parent_author, activity.permlink_permlink)" class="post-link">{{ activity.parent_permlink }}</a> 
           </div>
-          <div class="second-line"><div class="markdown-view mini-markdown">{{activity.body | postSummary}}</div></div>
+          <div class="second-line">
+            <div class="markdown-view mini-markdown reply-body" ng-click="goContent(activity.author, activity.permlink)" >{{activity.body | postSummary}}</div>
+          </div>
         </div>
         
         <!-- Mentions -->
         <div class="activity-content" ng-if="activity.type==='mention'" >
           <div class="first-line">
-            <a ng-click="goProfile(activity.source)" class="source-name">{{ activity.source }}</a>  
+            <a ng-click="goAccount(activity.source)" class="source-name">{{ activity.source }}</a>  
             <span class="activity-action label label-default">{{ 'mentioned' | __ }}</span>
           </div>
-          <div class="second-line"><a ng-click="goPost(activity.author, activity.permlink)" class="post-link">{{ activity.permlink }}</a> </div>
+          <div class="second-line"><a ng-click="goContent(activity.author, activity.permlink)" class="post-link">{{ activity.permlink }}</a> </div>
         </div>
         
         <!-- Follows -->
         <div class="activity-content" ng-if="['follow', 'unfollow', 'ignore'].includes(activity.type)">
           <div class="first-line">
-            <a ng-click="goProfile(activity.source)" class="source-name">{{ activity.source }}</a>  
+            <a ng-click="goAccount(activity.source)" class="source-name">{{ activity.source }}</a>  
           </div>
           <div class="second-line">
               <span ng-if="activity.type==='follow'" class="label label-success">following</span>
@@ -61,7 +63,7 @@ export default () => {
         <!-- Reblogs -->
         <div class="activity-content" ng-if="activity.type==='reblog'" >
           <div class="first-line">
-            <a ng-click="goProfile(activity.source)" class="source-name">{{ activity.source }}</a>  
+            <a ng-click="goAccount(activity.source)" class="source-name">{{ activity.source }}</a>  
             <span class="activity-action"><i class="fa fa-retweet"></i></span>
           </div>
           <div class="second-line"><a ng-click="goContent(activity.author, activity.permlink)" class="post-link">{{ activity.permlink }}</a></div>
@@ -69,15 +71,27 @@ export default () => {
         
       </div>
     </div>`,
-    controller: ($scope, $rootScope, $location, $filter, $window, eSteemService, activeUsername) => {
+    controller: ($scope, $rootScope, $location, steemService, eSteemService, activeUsername) => {
 
       moment.locale($rootScope.language);
 
-      $scope.clicked = (draft) => {
+      $scope.goAccount = (account) => {
+        let u = `/account/${account}`;
+        $location.path(u);
+      };
 
+      $scope.goContent = (account, permlink) => {
+        steemService.getContent(account, permlink).then(resp => {
+          if (resp.id) {
+            $rootScope.selectedPost = null;
+            const u = `/post/${resp.category}/${resp.author}/${resp.permlink}`;
+            $location.path(u);
+          }
+        });
       };
 
       $scope.date2key = function (s) {
+        // if not date return self
         if (s.split('-').length !== 3) {
           return s
         }
