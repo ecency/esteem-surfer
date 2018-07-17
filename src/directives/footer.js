a8c334272fb32a76bae4af781d45aa01b26a1267
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-07-17 13:53:22 +0300

    voting power added to footer #107

--- src/directives/footer.js
+++ src/directives/footer.js
@@ -1,5 +1,21 @@
 import {releasePost} from '../../package.json'
 
+const powerLevel = (l) => {
+  if (l >= 90) {
+    return 4;
+  }
+
+  if (l >= 80) {
+    return 3;
+  }
+
+  if (l >= 70) {
+    return 2;
+  }
+
+  return 1;
+};
+
 export default () => {
   return {
     restrict: 'AE',
@@ -9,7 +25,7 @@ export default () => {
 
     },
     templateUrl: 'templates/directives/footer.html',
-    controller: ($scope, $rootScope, $location, appVersion) => {
+    controller: ($scope, $rootScope, $interval, $timeout, $location, appVersion, activeUsername) => {
 
       $scope.version = appVersion;
 
@@ -25,7 +41,47 @@ export default () => {
         const [t, a, p] = releasePost.split('/');
         const u = `/post/${t}/${a.replace('@', '')}/${p}`;
         $location.path(u);
-      }
+      };
+
+      const resetPower = () => {
+        $rootScope.lastVp = 0;
+        $scope.vp = null;
+        $scope.vpLevel = 0;
+        $scope.vpFullIn = '';
+        $scope.notifyVp = false;
+      };
+
+      resetPower();
+
+      const votingPower = () => {
+        if (activeUsername() && $rootScope.userProps) {
+          $scope.vp = $rootScope.userProps.voting_power / 100;
+          $scope.vpLevel = powerLevel($scope.vp);
+          $scope.vpFullIn = Math.ceil((100 - $scope.vp) * 0.833333); // 100% / 120h = 0.833333h for %1
+
+          if ($rootScope.lastVp && $rootScope.lastVp !== $scope.vp) {
+            $scope.notifyVp = true;
+
+            $timeout(() => {
+              $scope.notifyVp = false;
+            }, 2000);
+          }
+
+          $rootScope.lastVp = $scope.vp;
+        } else {
+          resetPower();
+        }
+      };
+
+      $rootScope.$on('userLoggedOut', () => {
+        resetPower();
+      });
+
+      $rootScope.$on('userPropsRefreshed', () => {
+        votingPower();
+      });
+
+      votingPower();
     }
   };
 };
