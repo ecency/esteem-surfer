e77b7400eb13ccc6ec9514f28aee4242fa121b28
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-08-16 00:46:20 +0300

    checkpoint

---
+++ test/actions/trending-tags.spec.js
@@ -0,0 +1,16 @@
+import {spy} from 'sinon';
+import * as actions from '../../app/actions/trending-tags';
+
+describe('actions', () => {
+    it("should create 'fetch begin' action", () => {
+        expect(actions.fetchBegin()).toMatchSnapshot();
+    });
+
+    it("should create 'fetch ok' action", () => {
+        expect(actions.fetchOk(['life', 'kr', 'steemit', 'art'])).toMatchSnapshot();
+    });
+
+    it("should create 'fetch error' action", () => {
+        expect(actions.fetchError()).toMatchSnapshot();
+    });
+});
