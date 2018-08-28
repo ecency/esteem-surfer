d632fe2f81b753f9c7d2718c6fb5ee10625b9bef
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-08-28 19:21:00 +0300

    checkpoint

--- app/actions/post.js
+++ app/actions/post.js
@@ -1,47 +1,63 @@
 // @flow
-import {Client} from 'dsteem';
-import {makeGroupKeyForPosts} from '../utils/misc';
-import type {postStateType, postActionType} from '../reducers/types';
+import { Client } from 'dsteem';
+import { makeGroupKeyForPosts } from '../utils/misc';
+import type { postStateType, postActionType } from '../reducers/types';
 
-export const FETCH = 'FETCH';
-export const FETCH_COMPLETE = 'FETCH_COMPLETE';
-export const FETCH_ERROR = 'FETCH_ERROR';
-export const SET_READ = 'SET_CONTENT_READ';
-export const SET_VOTED = 'SET_CONTENT_VOTED';
+export const POSTS_FETCH_BEGIN = 'POSTS_FETCH_BEGIN';
+export const POSTS_FETCH_OK = 'POSTS_FETCH_OK';
+export const POSTS_FETCH_ERROR = 'POSTS_FETCH_ERROR';
+export const POST_SET_READ = 'POST_SET_READ';
+export const POST_SET_VOTED = 'POST_SET_VOTED';
 
 const client = new Client('https://api.steemit.com');
 
-export function fetchPosts(what, tag = '', startAuthor = '', startPermalink = '', limit = 20) {
-    return (dispatch: (action: postActionType) => void,
-            getState: () => postStateType) => {
-        const {post} = getState();
+export function fetchPosts(
+  what,
+  tag = '',
+  startAuthor = '',
+  startPermalink = '',
+  limit = 20
+) {
+  return (
+    dispatch: (action: postActionType) => void,
+    getState: () => postStateType
+  ) => {
+    const { post } = getState();
 
-        const groupKey = makeGroupKeyForPosts(what, tag);
+    const groupKey = makeGroupKeyForPosts(what, tag);
 
-        if (post.groups[groupKey] !== undefined) {
-            return
-        }
+    if (post.groups[groupKey] !== undefined) {
+      return;
+    }
 
+    dispatch({
+      type: POSTS_FETCH_BEGIN,
+      payload: { group: groupKey }
+    });
+
+    // make sure tag is not null or undefined. it should be empty string.
+    const query = {
+      tag: tag || '',
+      start_author: startAuthor,
+      start_permalink: startPermalink,
+      limit
+    };
+
+    client.database
+      .getDiscussions(what, query)
+      .then(resp => {
         dispatch({
-            type: FETCH,
-            payload: {group: groupKey},
+          type: POSTS_FETCH_OK,
+          payload: { data: resp, group: groupKey }
         });
 
-        // make sure tag is not null or undefined. it should be empty string.
-        const query = {tag: (tag || ''), start_author: startAuthor, start_permalink: startPermalink, limit};
-
-        client.database.getDiscussions(what, query).then((resp) => {
-            dispatch({
-                type: FETCH_COMPLETE,
-                payload: {data: resp, group: groupKey}
-            });
-
-            return resp
-        }).catch(() => {
-            dispatch({
-                type: FETCH_ERROR,
-                payload: {group: groupKey},
-            });
-        })
-    };
+        return resp;
+      })
+      .catch(() => {
+        dispatch({
+          type: POSTS_FETCH_ERROR,
+          payload: { group: groupKey }
+        });
+      });
+  };
 }
