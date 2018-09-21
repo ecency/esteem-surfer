62ba4ee467e450b9e79e01f3228289e1e3610684
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-09-21 17:23:20 +0300

    checkpoint

--- test/data/copy.js
+++ test/data/copy.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 
 const client = new Client('https://api.steemit.com');
 
-const author = 'good-karma';
-const permlink = 'esteem-spotlight-top-users-and-giveaway-16-afff4b320da3best';
+const author = process.argv[2];
+const permlink = process.argv[3];
 
 const error = msg => {
   console.log('\x1b[31m', msg, '\x1b[0m');
@@ -14,7 +14,7 @@ client.database
   .call('get_content', [author, permlink])
   .then(resp => {
     if (resp.id === 0) {
-      error('Content not found!');
+      error(`${author}/${permlink} not found!`);
       return;
     }
 
