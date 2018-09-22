4edf5a6e054f0e6510c275dfc9fcd5442bdc4e7b
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-09-22 21:47:45 +0300

    locale provider moved to App from Root

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,10 +1,30 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 
+// i18n
+import { addLocaleData, IntlProvider } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import tr from 'react-intl/locale-data/tr';
+
+import { flattenMessages } from '../utils';
+import messages from '../locales';
+
+addLocaleData([...en, ...tr]);
+
 class App extends React.Component {
   render() {
     const { children } = this.props;
-    return <React.Fragment>{children}</React.Fragment>;
+
+    const locale = 'en-US';
+
+    return (
+      <IntlProvider
+        locale={locale}
+        messages={flattenMessages(messages[locale])}
+      >
+        <React.Fragment>{children}</React.Fragment>
+      </IntlProvider>
+    );
   }
 }
 
--- app/containers/Root.js
+++ app/containers/Root.js
@@ -4,34 +4,18 @@ import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 
-// i18n
-import { addLocaleData, IntlProvider } from 'react-intl';
-import en from 'react-intl/locale-data/en';
-import tr from 'react-intl/locale-data/tr';
-
 import Routes from '../Routes';
-import { flattenMessages } from '../utils';
-import messages from '../locales';
-
-addLocaleData([...en, ...tr]);
 
 class Root extends Component {
   render() {
     const { store, history } = this.props;
 
-    const locale = 'en-US';
-
     return (
-      <IntlProvider
-        locale={locale}
-        messages={flattenMessages(messages[locale])}
-      >
-        <Provider store={store}>
-          <ConnectedRouter history={history}>
-            <Routes />
-          </ConnectedRouter>
-        </Provider>
-      </IntlProvider>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Routes />
+        </ConnectedRouter>
+      </Provider>
     );
   }
 }
