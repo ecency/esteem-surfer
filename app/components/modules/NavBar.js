906ff4d2f50037de8994b1f0ec5704ca10838162
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-09-23 20:11:29 +0300

    working on settings. checkpoint

--- app/components/modules/NavBar.js
+++ app/components/modules/NavBar.js
@@ -1,16 +1,15 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import PropTypes from 'prop-types';
 
-import {Tooltip, Modal} from 'antd';
-import {FormattedMessage} from 'react-intl';
+import { Tooltip, Modal } from 'antd';
+import { FormattedMessage } from 'react-intl';
 
 import Mi from '../elements/Mi';
 
-import Settings from '../elements/Settings'
-
+import Settings from '../elements/Settings';
 
 export const checkPathForBack = path => {
   if (!path) {
@@ -42,45 +41,45 @@ class NavBar extends Component {
   };
 
   goBack = () => {
-    const {history} = this.props;
+    const { history } = this.props;
 
     history.goBack();
   };
 
   goForward = () => {
-    const {history} = this.props;
+    const { history } = this.props;
 
     history.goForward();
   };
 
   refresh = () => {
-    const {reloadFn} = this.props;
+    const { reloadFn } = this.props;
 
     reloadFn();
   };
 
   favorite = () => {
-    const {favoriteFn} = this.props;
+    const { favoriteFn } = this.props;
 
     if (favoriteFn) favoriteFn();
   };
 
   bookmark = () => {
-    const {bookmarkFn} = this.props;
+    const { bookmarkFn } = this.props;
 
     if (bookmarkFn) bookmarkFn();
   };
 
   changeTheme = () => {
-    const {actions} = this.props;
-    const {changeTheme} = actions;
+    const { actions } = this.props;
+    const { changeTheme } = actions;
 
     changeTheme();
   };
 
   logoClicked = () => {
-    const {location, global} = this.props;
-    const {selectedFilter} = global;
+    const { location, global } = this.props;
+    const { selectedFilter } = global;
 
     const newLoc = `/${selectedFilter}`;
 
@@ -89,7 +88,7 @@ class NavBar extends Component {
       return;
     }
 
-    const {history} = this.props;
+    const { history } = this.props;
     history.push(newLoc);
   };
 
@@ -104,7 +103,7 @@ class NavBar extends Component {
       postBtnActive
     } = this.props;
 
-    const {settingsModalVisible} = this.state;
+    const { settingsModalVisible } = this.state;
 
     let canGoBack = false;
     if (history.entries[history.index - 1]) {
@@ -131,7 +130,7 @@ class NavBar extends Component {
           />
           <div className={`btn-post-mini  ${postBtnActive ? 'visible' : ''}`}>
             <span className="icon">
-              <Mi icon="edit"/>
+              <Mi icon="edit" />
             </span>
           </div>
           <div className="nav-controls">
@@ -140,26 +139,26 @@ class NavBar extends Component {
               onClick={() => this.goBack()}
               role="none"
             >
-              <Mi icon="arrow_back"/>
+              <Mi icon="arrow_back" />
             </a>
             <a
               className={forwardClassName}
               onClick={() => this.goForward()}
               role="none"
             >
-              <Mi icon="arrow_forward"/>
+              <Mi icon="arrow_forward" />
             </a>
             <a
               className={reloadClassName}
               onClick={() => this.refresh()}
               role="none"
             >
-              <Mi icon="refresh"/>
+              <Mi icon="refresh" />
             </a>
           </div>
           <div className="address-bar">
             <div className="pre-add-on">
-              <Mi icon="search"/>
+              <Mi icon="search" />
             </div>
             <div className="address">
               <span className="protocol">esteem://</span>
@@ -171,7 +170,7 @@ class NavBar extends Component {
                 onClick={() => this.favorite()}
                 role="none"
               >
-                <Mi icon="star_border"/>
+                <Mi icon="star_border" />
               </a>
             ) : (
               ''
@@ -182,7 +181,7 @@ class NavBar extends Component {
                 onClick={() => this.bookmark()}
                 role="none"
               >
-                <Mi icon="bookmark"/>
+                <Mi icon="bookmark" />
               </a>
             ) : (
               ''
@@ -196,7 +195,7 @@ class NavBar extends Component {
               }}
               role="none"
             >
-              <Mi icon="brightness_medium"/>
+              <Mi icon="brightness_medium" />
             </a>
             <a
               className="settings"
@@ -205,7 +204,7 @@ class NavBar extends Component {
               }}
               role="none"
             >
-              <Mi icon="settings"/>
+              <Mi icon="settings" />
             </a>
           </div>
           <div className="user-menu">
@@ -215,7 +214,7 @@ class NavBar extends Component {
               mouseEnterDelay={2}
             >
               <a className="login">
-                <Mi icon="account_circle"/>
+                <Mi icon="account_circle" />
               </a>
             </Tooltip>
           </div>
@@ -225,8 +224,10 @@ class NavBar extends Component {
           onCancel={this.onSettingsModalCancel}
           footer={false}
           width="60%"
-          title={<FormattedMessage id="settings.title"/>}
-          centered>
+          title={<FormattedMessage id="settings.title" />}
+          destroyOnClose
+          centered
+        >
           <Settings {...this.props} />
         </Modal>
       </div>
