376ddd0f02ba9658214f4498019702f5ee5bff00
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-09-25 21:04:16 +0300

    working on pin code. checkpoint

--- app/components/dialogs/PinCreate.js
+++ app/components/dialogs/PinCreate.js
@@ -1,36 +1,138 @@
-import React, {Component} from 'react';
-
-
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import CryptoJS from 'crypto-js';
 
-import {Modal} from 'antd';
-import {FormattedHTMLMessage, FormattedMessage, injectIntl} from 'react-intl';
+import { Modal, Input, Col, Button } from 'antd';
+import { FormattedMessage, injectIntl } from 'react-intl';
 
+import { getItem, setItem } from '../../helpers/storage';
+import logo from '../../img/logo-big.png';
+import defaults from '../../constants/defaults';
 
 class PinCreate extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      dialogVisible: false,
+      value: ''
+    };
+
+    this.intId = null;
   }
 
   componentDidMount() {
+    this.intId = setInterval(() => {
+      const { dialogVisible } = this.state;
 
+      if (dialogVisible) {
+        return;
+      }
+
+      const code = getItem('pin-code');
+
+      if (!code) {
+        this.setState({ dialogVisible: true });
+      }
+    }, 1000);
   }
 
+  componentWillUnmount() {
+    clearInterval(this.intId);
+  }
 
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  submitForm() {
+    const { value } = this.state;
+
+    const hashed = CryptoJS.MD5(value).toString();
+
+    setItem('pin-code', hashed);
+    this.setState({ dialogVisible: false });
+  }
 
   render() {
-    const {global} = this.props;
-
+    const { intl } = this.props;
+    const { dialogVisible, value } = this.state;
 
     return (
-      null
+      <Modal
+        footer={null}
+        closable={false}
+        keyboard={false}
+        width="500px"
+        centered
+        destroyOnClose
+        afterClose={() => {
+          console.log('closed');
+        }}
+        visible={dialogVisible}
+      >
+        <div className="pin-create-dialog-content">
+          <div className="dialog-form">
+            <div className="brand">
+              <img src={logo} alt="" />
+            </div>
+            <div className="form-content">
+              <h2 className="form-title">
+                <FormattedMessage id="create-pin-code.title" />
+              </h2>
+              <p className="form-desc">
+                <FormattedMessage id="create-pin-code.desc1" />
+              </p>
+              <Input.Group>
+                <Col span={18}>
+                  <Input
+                    autoFocus
+                    type="password"
+                    maxLength={20}
+                    placeholder={intl.formatMessage({
+                      id: 'create-pin-code.input-placeholder'
+                    })}
+                    value={value}
+                    onChange={e => {
+                      this.handleChange(e);
+                    }}
+                    onKeyUp={e => {
+                      if (
+                        e.key === 'Enter' &&
+                        value.length >= defaults.minPinCode
+                      ) {
+                        this.submitForm();
+                      }
+                    }}
+                  />
+                </Col>
+                <Col span={3}>
+                  <Button
+                    type="primary"
+                    shape="circle"
+                    disabled={value.length < defaults.minPinCode}
+                    onClick={() => {
+                      this.submitForm();
+                    }}
+                  >
+                    <i className="mi">lock</i>
+                  </Button>
+                </Col>
+              </Input.Group>
+              <p className="form-desc2">
+                <FormattedMessage id="create-pin-code.desc2" />
+              </p>
+            </div>
+          </div>
+        </div>
+      </Modal>
     );
   }
 }
 
 PinCreate.propTypes = {
-  global: PropTypes.shape({}).isRequired
+  intl: PropTypes.instanceOf(Object).isRequired
 };
 
 export default injectIntl(PinCreate);
--- app/containers/App.js
+++ app/containers/App.js
@@ -1,30 +1,32 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-
-import PinCreate from '../components/dialogs/PinCreate'
+import { connect } from 'react-redux';
 
 // i18n
-import {addLocaleData, IntlProvider} from 'react-intl';
+import { addLocaleData, IntlProvider } from 'react-intl';
 import en from 'react-intl/locale-data/en';
 import tr from 'react-intl/locale-data/tr';
 
-import {flattenMessages} from '../utils';
+import PinCreate from '../components/dialogs/PinCreate';
+
+import { flattenMessages } from '../utils';
 import messages from '../locales';
 
 addLocaleData([...en, ...tr]);
 
 class App extends React.Component {
   render() {
-    const {children, global} = this.props;
-    const {locale} = global;
+    const { children, global } = this.props;
+    const { locale } = global;
 
     return (
       <IntlProvider
         locale={locale}
         messages={flattenMessages(messages[locale])}
       >
-        <React.Fragment>{children} <PinCreate {...this.props} /></React.Fragment>
+        <React.Fragment>
+          {children} <PinCreate {...this.props} />
+        </React.Fragment>
       </IntlProvider>
     );
   }
