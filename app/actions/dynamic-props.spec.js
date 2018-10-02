9da443445a4817057466bc80b5ea3637994aa1c2
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-10-02 21:02:21 +0300

    dynamic-props action creator test added

---
+++ app/actions/dynamic-props.spec.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import * as actions from './dynamic-props';
+
+describe('dynamic props actions', () => {
+  it('(1) fetched action creator', () => {
+    const steemPerMVests = 495.05469644322403;
+    const base = 0.86;
+    const fundRecentClaims = '497885072157522967';
+    const fundRewardBalance = 834665.854;
+
+    expect(
+      actions.fetched(steemPerMVests, base, fundRecentClaims, fundRewardBalance)
+    ).toMatchSnapshot();
+  });
+});
