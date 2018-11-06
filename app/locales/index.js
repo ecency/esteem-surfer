b82c74f55c369b066971aaf7ca3a1a11f3f9f637
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-11-06 23:01:22 +0000

    pls add lang file after completed

--- app/locales/index.js
+++ app/locales/index.js
@@ -2,12 +2,10 @@
 
 export default {
   'en-US': require('./en-US.json'),
-  'en-RU': require('./en-RU.json'),
   'tr-TR': require('./tr-TR.json')
 };
 
 export const locales = [
   { id: 'en-US', name: 'English' },
-  { id: 'en-RU', name: 'Русский' },
   { id: 'tr-TR', name: 'Türkçe' }
 ];
