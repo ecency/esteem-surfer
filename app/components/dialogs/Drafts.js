3efd11158885876d03544da5ef88203757e0abb3
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-12-13 16:04:29 +0000

    drafts user reputation fix

--- app/components/dialogs/Drafts.js
+++ app/components/dialogs/Drafts.js
@@ -50,7 +50,10 @@ class DraftListItem extends Component {
               <UserAvatar user={author} size="small" />
             </div>
             <div className="author">
-              {author} <span className="author-reputation">{reputation}</span>
+              {author}{' '}
+              {reputation && (
+                <span className="author-reputation">{reputation}</span>
+              )}
             </div>
           </div>
           <a className="category">{tag}</a>
@@ -111,11 +114,15 @@ class DraftListItem extends Component {
   }
 }
 
+DraftListItem.defaultProps = {
+  reputation: null
+};
+
 DraftListItem.propTypes = {
   activeAccount: PropTypes.instanceOf(Object).isRequired,
   onDelete: PropTypes.func.isRequired,
   author: PropTypes.string.isRequired,
-  reputation: PropTypes.number.isRequired,
+  reputation: PropTypes.number,
   item: PropTypes.instanceOf(Object).isRequired,
   history: PropTypes.instanceOf(Object).isRequired,
   intl: PropTypes.instanceOf(Object).isRequired
@@ -173,7 +180,9 @@ class Drafts extends Component {
 
     const { username: author } = activeAccount;
     const { accountData } = activeAccount;
-    const reputation = authorReputation(accountData.reputation);
+    const reputation = accountData
+      ? authorReputation(accountData.reputation)
+      : null;
 
     return (
       <div className="drafts-dialog-content">
