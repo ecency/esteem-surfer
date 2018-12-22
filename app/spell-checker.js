eced7fca03a9efc6bdb0e0c245337e22a70ecdd6
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-12-22 22:37:05 +0000

    spell checker performance improvements

--- app/spell-checker.js
+++ app/spell-checker.js
@@ -1,10 +1,22 @@
 import { SpellCheckHandler } from 'electron-spellchecker';
 
+const spCache = {};
+
 const spellChecker = new SpellCheckHandler();
 
 spellChecker.switchLanguage('en-US');
 
-window.isMisspelled = str => spellChecker.isMisspelled(str);
+window.isMisspelled = str => {
+  if (spCache[str]) {
+    return spCache[str];
+  }
+
+  const r = spellChecker.isMisspelled(str);
+
+  spCache[str] = r;
+
+  return r;
+};
 
 window.getSpellingCorrections = text =>
   spellChecker.getCorrectionsForMisspelling(text);
