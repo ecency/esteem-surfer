9515bb5f08fcfaa15fd1dcdaf44eeb1145ca3d87
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2019-01-28 19:29:37 +0300

    locale files added

--- app/locales/index.js
+++ app/locales/index.js
@@ -2,10 +2,20 @@
 
 export default {
   'en-US': require('./en-US.json'),
-  'ru-RU': require('./ru-RU.json')
+  'ru-RU': require('./ru-RU.json'),
+  'es-ES': require('./es-ES.json'),
+  'pt-PT': require('./pt-PT.json'),
+  'pl-PL': require('./pl-PL.json'),
+  'hu-HU': require('./hu-HU.json'),
+  'nl-NL': require('./nl-NL.json')
 };
 
 export const locales = [
   { id: 'en-US', name: 'English' },
-  { id: 'ru-RU', name: 'Русский' }
+  { id: 'ru-RU', name: 'Русский' },
+  { id: 'es-ES', name: 'Español' },
+  { id: 'pt-PT', name: 'Português' },
+  { id: 'pl-PL', name: 'Polski' },
+  { id: 'hu-HU', name: 'Magyar' },
+  { id: 'nl-NL', name: 'Dutch' }
 ];
